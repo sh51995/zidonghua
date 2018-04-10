eddc56147b3cecc93c7dfa25fc461610f5b86c4d
sh51995 <sh51995@163.com>
2018-04-10 10:07:17 +0800

    222222222222

--- automatedtesting/src/util/CmdUtil.java
+++ automatedtesting/src/util/CmdUtil.java
@@ -4,22 +4,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class CmdUtil {
-	static Process p;
-	  public static String CMD(Process p,String cmd) {
+	  public static String CMD(String cmd) {
 		  String result="";
           BufferedReader br = null;
           try {
-              p = Runtime.getRuntime().exec(cmd);
+        	  Process    p = Runtime.getRuntime().exec(cmd);
               br = new BufferedReader(new InputStreamReader(p.getInputStream()));
               String line = null;
-              String s=null;
+              String s="";
               while ((line = br.readLine()) != null) {
             	  s=s+line;
                   System.out.println(line);
               }
               result=s;
               System.out.println(s);
-              System.out.println("==========");
           } catch (Exception e) {
               e.printStackTrace();
           } finally {
@@ -38,6 +36,7 @@ public class CmdUtil {
 	  
 	  public static void main(String[] args) {
 	//	CMD("adb shell dumpsys meminfo com.gl365.android.member");
-		CMD(p,"cmd.exe /c node C:/\"Program Files (x86)\"/Appium/node_modules/appium/bin/appium.js");
+		String s=CMD("adb devices");
+		System.out.println(s);
 	}
 }
